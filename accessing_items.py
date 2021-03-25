09b480b8174dc3ef5ea2d5f44e416388822835d5
Reena Kumari <reena20@navgurukul.org>
2021-03-25 11:54:30 +0530

    first commit

---
+++ accessing_items.py
@@ -0,0 +1,50 @@
+#first questions:-
+"""thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
+print(thisdict["brand"])
+print(thisdict["year"])
+
+y=thisdict.get('model')
+print(y)
+
+a=thisdict.keys()
+print(a)
+
+n=thisdict.get("year")
+print(n)"""
+
+#second questions:-
+"""a={
+    101:"Reena kumari",
+    102:"sana",
+    103:"Amrita"
+    }
+print(a)
+print(a.get(101))
+print(a.get(102))"""
+
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+car[101]="red"
+
+print(car)
+
+print(x) #after the change
+
+for x in car:
+    print(x)
